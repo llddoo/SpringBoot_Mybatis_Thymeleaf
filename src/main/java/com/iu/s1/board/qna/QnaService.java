e8038ca0da71da3a9173229ecf52d8e483a06483
이석준 <llddoo@naver.com>
2021-05-14 02:07:47 +0900

    dd

--- src/main/java/com/iu/s1/board/qna/QnaService.java
+++ src/main/java/com/iu/s1/board/qna/QnaService.java
@@ -1,9 +1,14 @@
 package com.iu.s1.board.qna;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.iu.s1.board.BoardService;
+import com.iu.s1.board.BoardVO;
 import com.iu.s1.util.FileManager;
+import com.iu.s1.util.Pager;
 
 public class QnaService implements BoardService {
 
@@ -12,5 +17,31 @@ public class QnaService implements BoardService {
 	@Autowired
 	private FileManager fileManager;
 	
+	@Override
+	public List<BoardVO> getList(Pager pager) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public BoardVO getSelect(BoardVO boardVO) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public int setInsert(BoardVO boardVO, MultipartFile[] files) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public int setDelete(BoardVO boardVO) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public int setUpdate(BoardVO boardVO) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 	
 }
