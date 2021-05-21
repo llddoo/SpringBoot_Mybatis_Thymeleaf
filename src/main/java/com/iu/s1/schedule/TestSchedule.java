21364645a6294f08b57bf5e9c4337f04079d1f97
llddoo <74532161+llddoo@users.noreply.github.com>
2021-05-21 17:53:47 +0900

    dsffd

---
+++ src/main/java/com/iu/s1/schedule/TestSchedule.java
@@ -0,0 +1,28 @@
+package com.iu.s1.schedule;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.iu.s1.member.MemberMapper;
+import com.sun.swing.internal.plaf.metal.resources.metal;
+
+@Component
+public class TestSchedule {
+	
+	@Autowired
+	private MemberMapper memberMapper;
+	
+	@Scheduled(fixedRateString = "1000", initialDelayString = "2000")
+	public void fixRateScheduleTest()throws Exception{
+		System.out.println("fixRateSchedule");
+	}
+	
+	@Scheduled(cron = "0 0 9-18/2 * * 1-5")
+	public void cronTest()throws Exception{
+		System.out.println("Cron ~~~~~~");
+	}
+	
+	
+
+}
